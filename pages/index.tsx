7d37e858659a3b85e8b72f3185ba619dd3d2c565
graemen16 <graemen16@gmail.com>
2023-03-26 12:29:36 +0100

    delete getServerSideProps()

--- pages/index.tsx
+++ pages/index.tsx
@@ -137,7 +137,7 @@ return (
   )
 }
 
-export async function getServerSideProps() {
-  const data = JSON.stringify({ time: new Date() });
-  return { props: { data } };
-}
+//export async function getServerSideProps() {
+//  const data = JSON.stringify({ time: new Date() });
+//  return { props: { data } };
+//}
